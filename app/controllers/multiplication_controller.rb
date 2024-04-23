7cb9722b4818c7740687525e1ecd18b138c336f8
fpothuc <164956630+fpothuc@users.noreply.github.com>
2024-04-23 17:20:40 +0000

    fixing typos

--- app/controllers/multiplication_controller.rb
+++ app/controllers/multiplication_controller.rb
@@ -5,10 +5,10 @@ class MultiplicationController > application_controller
 
   def multiply_these
     @first_number = params.fetch("first_number").to_f
-    @second_number = params.fetch("second_num").to_f
+    @second_number = params.fetch("second_number").to_f
 
     @result =  @first_number * @second_number
 
-    render({ :template => "multiply_templates/multiply_results" })
+    render({ :template => "multiplication_templates/multiply_results" })
   end
 end
